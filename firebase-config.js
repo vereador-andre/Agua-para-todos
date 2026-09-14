592cca13e3bdc125a36650e6b9e112c55c903ed3
vereador-andre <andrezinhok@gmail.com>
2026-09-14 11:16:53 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,16 @@
-// Cole aqui a configuração do seu aplicativo Web do Firebase.
-// Firebase Console > Project settings (Configurações do projeto) > Your apps > SDK setup and configuration.
-// NUNCA coloque aqui uma chave privada/service account. Este arquivo contém apenas a configuração pública do app.
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-export const firebaseConfig = {
+const firebaseConfig = {
   apiKey: "AIzaSyBuftlms0LcdZrM9dpcf5wIk7eWjK3_o4A",
   authDomain: "agua-para-todos-19454.firebaseapp.com",
-  projectId: "agua-para-todos-19454",
+  projectId: "water-for-all-19454",
   storageBucket: "agua-para-todos-19454.firebasestorage.app",
   messagingSenderId: "799145802902",
   appId: "1:799145802902:web:b40f090db609b71c3696b7"
 };
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { app, db };
